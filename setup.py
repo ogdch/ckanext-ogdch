ce78a31b91d2bea0839499f3dbf7b820cb025d5e
Philipp Küng <git@philippkueng.ch>
2013-06-13 11:31:01 +0200

    Revert "removed namespace packages since they're causing an error during installation, maybe TBF"
    This reverts commit 5b1731823539754ea83cb906184d436560126223.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     url='http://www.liip.ch',
     license='GPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.ogdch'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
